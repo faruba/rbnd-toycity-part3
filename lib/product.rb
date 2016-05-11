e9347170f6b1fe537b8b98d06952e869325ec4a6
faruba <farubafaruba@126.com>
2016-05-11 14:19:03 +0800

    throw error when exists

--- lib/product.rb
+++ lib/product.rb
@@ -1,7 +1,18 @@
+require_relative "./errors"
+
 class Product
+	attr_reader :title
 	@@products = []
 	def initialize(options={})
-		@@products << self 
+		@title = options[:title]
+		add_to_products
+	end
+	def add_to_products
+		if @@products.any? { |product| product.title == @title}
+			raise DuplicateProductError , "#{@title} already exists."
+		else
+			@@products << self 
+		end
 	end
 	
 	def self.all
