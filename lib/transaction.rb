9308b637ef79523eb7785800547b24e089e38511
faruba <farubafaruba@126.com>
2016-05-11 16:58:20 +0800

    Transaction init

--- lib/transaction.rb
+++ lib/transaction.rb
@@ -1,11 +1,23 @@
-class Transaction
+require_relative "./set"
+
+class Transaction < SetContainer
 	@@uuid = 0
+	@@transactions = []
 	attr_reader :customer, :product, :id
 	def initialize(customer, product)
 		@@uuid += 1
 		@id = @@uuid
 		@customer = customer 
 		@product = product
+		SetContainer.add_to(@@transactions, self)
+	end
+
+	def is_same?(trans)
+		return trans.id == @id
+	end
+
+	def self.all
+		@@transactions
 	end
 end
 
